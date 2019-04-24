bc531332dae1beac82a0c6c852cc9afd8177997a
eemc2oops <yc.c@outlook.com>
2019-04-24 18:12:16 +0800

    春秋城市列表

--- y_chunqiu.py
+++ y_chunqiu.py
@@ -3,44 +3,86 @@ from urllib.parse import unquote
 from urllib.parse import urlencode
 import requests
 import re
+import json
+import string
 from selenium import webdriver
+import datetime
 
 import y_log
 
 w2l = y_log.clog(__name__ == "__main__")
 
+#time.strftime('%Y-%m-%d',time.localtime(time.time()))
+
 class chunqiu_air:
     '''春秋'''
-    def __init__(self):
-        pass
-
-    def _city_list(self):
+    def _get_city_list(self):
         url = 'https://ajax.springairlines.com/cache/js/modules/data/citydict-zh-cn.js?vs=v2019041921'
         r = requests.get(url)
         if r.status_code != requests.codes.ok:
             w2l.error('{0} url {1} return {2}'.format(type(self), url, r.status_code))
             return
 
-        print(requests.codes.ok)
-        #print(r.text)
-        #fmt = re.compile('define(.*?)')
         fmt = re.compile('.*?return (.*?)}\).*?$') 
-        #txt = fmt.findall(r.text)
-        #for txt in fmt.findall(r.text):
-        #    print('============================')
-        #    print(txt)
+        txts = fmt.findall(r.text)
+        if len(txts) != 1:
+            w2l.error('{0} get city list failed. {1}'.format(type(self), len(txts)))
+            return
 
-    def _city(self, city_from, city_to):
-        pass
+        data_list = json.loads(txts[0])
 
-    def tickets(self, data, city_from, city_to):
+        city_list = []
+        for k,v in data_list.items():
+            #k,v  'FOC' , ['FuZhou', '福州', 'FZ|中国|三坊七巷|鼓山|zhongguo|sanfangqixiang|gushan', 'FOC', '中国']
+            #city = {}
+            #city['code'] = k
+            #city['chs'] = v[1]
+            #city['eng'] = v[0]
+            #city['country'] = v[4]
+            
+            city = {
+                'code' : k,
+                'chs' : v[1],
+                'eng' : v[0],
+                'country' : v[4],
+            }
+
+            #print('city[%s] %s(%s) %s' % (city['code'], city['chs'], city['eng'], city['country']))
+
+            city_list.append(city)
+
+        if len(city_list) == 0:
+            w2l.error('{0} get empty city list.'.format(type(self)))
+            return
+
+        self.city_list = city_list
+
+    def __init__(self):
+        self.city_list = []
+        self._get_city_list()
+
+    def show_city_list(self):
+        for item in self.city_list:
+            print(item)
+
+    def _get_airport(self, city):
+        '''跟据城市获取机场信息'''
+        for item in self.city_list:
+            if item['chs'].find(city) != -1:
+                yield item
+
+    def _tickets(self, city_from, city_to):
+        '''尽可能多的获取信息'''
         data = {
-            'Departure' : '武汉',
-            'Arrival' : '东京(成田)',
+            #'Departure' : '武汉',
+            #'Arrival' : '东京(成田)',
+            'Departure' : city_from,
+            'Arrival' : city_to,
             'Active9s' : '0',
             'Currency' : '0',
             'SType' : '10',
-            'DepartureDate' : '2019-04-26',
+            #'DepartureDate' : '2019-04-26',
+            'DepartureDate' : (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d'),
             'ReturnDate' : '',
             'IsIJFlight' : 'true',
             'IsBg' : 'false',
@@ -49,7 +91,8 @@ class chunqiu_air:
             'IfRet' : 'false',
             'IsShowTaxprice' : 'true',
             'IsUM' : 'false',
-            'Days' : '61',
+            #'Days' : '61',
+            'Days' : '5',
         }
         
         url = 'https://flights.ch.com/Flights/MinPriceTrends'
@@ -80,9 +123,15 @@ class chunqiu_air:
         #    print(key + ' = ' + value)
         print(r.text)
 
+    def tickets(self, city_from, city_to):
+        for city1 in self._get_airport(city_from):
+            for city2 in self._get_airport(city_to):
+                self._tickets(city1['code'], city2['code'])
+
     def test(self):
-        self._city_list()
-        #ch.tickets('20190808', ['WUH'], ['TK'])
+        #self._city_list()
+        #self.show_city_list()
+        self.tickets('武汉', '东京') 
 
 def run_entry():
 
